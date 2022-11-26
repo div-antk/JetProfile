1fb15e341d30254ec87179437ae369085722c5eb
div-antk <51283468+owaii@users.noreply.github.com>
2022-11-26 23:59:00 +0900

    ダークモードを無効化

--- app/src/main/java/com/example/jetprofile/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetprofile/ui/theme/Theme.kt
@@ -28,15 +28,9 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun JetProfileTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun JetProfileTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
