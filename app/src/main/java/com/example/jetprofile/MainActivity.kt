6e1b25ce54e4a252ce91f07d9ae99b283f2a79a4
div-antk <51283468+owaii@users.noreply.github.com>
2022-11-29 23:34:13 +0900

    テキストを中央寄せにする

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -42,7 +43,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Profile() {
-    Column() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
         // 名前
         Text(
             text = "あんどた",
